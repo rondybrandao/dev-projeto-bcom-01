97d714a199102766e20cd391aeaf042b6d48ea8d
rondy brandao <rondynely@hotmail.com>
2017-11-05 13:52:17 -0400

    fist commit producao 05/11/2017 13:51

--- Desenvolvimento_producao/wsgi.py
+++ Desenvolvimento_producao/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Desenvolvimento_teste project.
+WSGI config for Desenvolvimento_producao project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Desenvolvimento_teste.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Desenvolvimento_producao.settings")
 
 application = get_wsgi_application()
